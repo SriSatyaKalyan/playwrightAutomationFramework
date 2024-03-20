b682b15b2e2824f343e9c90bb6fc81eb9658a4c7
Satya Kalyan <satyakalyan5@gmail.com>
2024-03-20 14:50:17 -0600

    Added Clear Items from Wishlist test

--- tests/itemActions.spec.js
+++ tests/itemActions.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require("@playwright/test");
-const { assert } = require("console");
-const exp = require("constants");
+// Annotate entire file as serial.
+test.describe.configure({ mode: "serial" });
 
 test("Add Items to Wishlist - Search Page", async ({ browser }) => {
 	const context = await browser.newContext();
@@ -56,7 +56,6 @@ test("Add Items to Wishlist - Item Page", async ({ browser }) => {
 		.first()
 		.click();
 
-	// await page.pause();
 	await page.waitForTimeout(2_000);
 	await page
 		// .locator("//div[@class='product-social-links']")
@@ -72,17 +71,48 @@ test("Add Items to Wishlist - Item Page", async ({ browser }) => {
 	await expect(page).toHaveTitle("My Wish List");
 
 	await expect(page.getByText("Dash Digital Watch").first()).toBeVisible();
-
-	// await page.pause();
 });
 
+test("Clear Items in Wish List", async ({ browser }) => {
+	const context = await browser.newContext();
+	const page = await context.newPage();
+
+	await page.goto("https://magento.softwaretestingboard.com/");
+	await page.goto(
+		"https://magento.softwaretestingboard.com/wishlist/index/index/"
+	);
+
+	await page
+		.locator("//input[@name='login[username]']")
+		.fill("liam.k@mail.com");
+	await page.locator("//input[@name='login[password]']").fill("MediP@ss");
+	await page.locator("//button[@name='send']").first().click();
+
+	await expect(page).toHaveTitle("My Wish List");
+	let noOfItems = await page
+		.locator("//span[@class='toolbar-number']")
+		.first()
+		.textContent();
+
+	noOfItems = noOfItems.substring(1, 2);
+	console.log("The number of items in the Wish List are: " + noOfItems);
+
+	for (let i = 0; i < noOfItems; i++) {
+		await page.locator("//div[@class='product-item-info']").first().hover();
+		await page.waitForTimeout(3_000);
+		await page.locator("//a[@data-role='remove']").first().click();
+	}
+
+	await expect(
+		page.locator("//div[@class='message info empty']")
+	).toBeVisible();
+});
 
 // test("Compare Products - Add To Compare", async ({ browser }) => {
 // 	const context = await browser.newContext();
 // 	const page = await context.newPage();
 
 // 	await page.goto("https://magento.softwaretestingboard.com/");
-
 
 // });
 
@@ -94,13 +124,6 @@ test("Add Items to Wishlist - Item Page", async ({ browser }) => {
 // });
 
 // test("Increase Items per Page Count", async ({ browser }) => {
-// 	const context = await browser.newContext();
-// 	const page = await context.newPage();
-
-// 	await page.goto("https://magento.softwaretestingboard.com/");
-// });
-
-// test("Clear Items in Wish List", async ({ browser }) => {
 // 	const context = await browser.newContext();
 // 	const page = await context.newPage();
 
